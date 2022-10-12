d295bd8c20b1a7e04d53011b62006727cfe6a1bc
Avgust Zagorodnikov <zagavg@yandex.ru>
2022-10-12 12:04:40 +0300

    Update

---
+++ Cube.cs
@@ -0,0 +1,7 @@
+class Cube
+{
+    public Cube()
+    {
+
+    }
+}
---
+++ RemontData.cs
@@ -0,0 +1,14 @@
+class RemontDataElement
+{
+    public RemontDataElement(string[] resources, Effect[] effect)
+    {
+
+    }
+}
+class RemontData
+{
+    public RemontData(RemontDataElement[] remontDataElements)
+    {
+
+    }
+}
