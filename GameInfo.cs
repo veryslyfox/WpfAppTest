5f1d7afe4ef1080fe3c0e807347e470aa56595f4
Avgust Zagorodnikov <zagavg@yandex.ru>
2022-10-14 14:37:25 +0300

    Update

---
+++ GameInfo.cs
@@ -0,0 +1,17 @@
+class GameInfo
+{
+    public GameInfo(PersonageInfo personage)
+    {
+
+    }
+    public string Unit => "МБ";
+
+    public double DataGeneration()
+    {
+        return 0;
+    }
+    public void Save()
+    {
+        
+    }
+}
