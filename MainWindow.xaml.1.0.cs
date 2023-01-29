511b6b8f0f36582983c17b5655c4d740f40731f7
Avgust Zagorodnikov <zagavg@yandex.ru>
2023-01-29 08:30:07 +0300

    Update

--- MainWindow.xaml.1.0.cs
+++ MainWindow.xaml.1.0.cs
@@ -32,7 +32,7 @@ public partial class MainWindow : Window
         InitializeComponent();
         _bitmap = new((int)image.Width, (int)image.Height, 96, 100, PixelFormats.Bgr32, null);
         image.Source = _bitmap;
-        _timer.Interval = TimeSpan.FromSeconds(0.00001);
+        _timer.Interval = TimeSpan.FromSeconds(0.1);
         _timer.Tick += Tick;
         _timer.Start();
         MouseLeftButtonDown += ButtonHandler;
@@ -54,29 +54,63 @@ public partial class MainWindow : Window
     {
         return Color.FromRgb(Saturate(r), Saturate(g), Saturate(b));
     }
+    public Color Normalize(Color color, byte lightness)
+    {
+        var r = color.R;
+        var g = color.G;
+        var b = color.B;
+        var max = Math.Max(r, Math.Max(g, b));
+        if (max == 0)
+        {
+            return Color.FromRgb(lightness, lightness, lightness);
+        }
+        var normalizer = (double)lightness / max;
+        return FromSRgb((int)(r * normalizer), ((int)(g * normalizer)), ((int)(b * normalizer)));
 
+    }
+    public Color HueToRgb(int h)
+    {
+        var result = new Color();
+        var hue = h % 360;
+        var hv = (hue % 120) * 256 / 120;
+        var a = 0;
+        var b = hv;
+        var c = 255 - hv;
+        var d = 255;
+        void DoubleInterval(int min, int max, int r, int g, int b)
+        {
+            if (min < hue && max < hue)
+            {
+                result = FromRgb(r, g, b);
+            }
+        }
+        DoubleInterval(0, 60, d, b, a);
+        DoubleInterval(60, 120, c, d, a);
+        DoubleInterval(120, 180, a, d, b);
+        DoubleInterval(180, 240, a, c, b);
+        DoubleInterval(240, 300, b, a, d);
+        DoubleInterval(300, 360, d, a, c);
+        return result;
+    }
     private void Tick(object? sender, EventArgs e)
     {
-        BinaryMap map = new BinaryMap(new Style(20, 20, FromRgb(255, 255, 255)), 40, 40);
-        map.OnRandom(0.001);
+
         _bitmap.Lock();
+
         for (int y = 0; y < _bitmap.PixelHeight; y++)
         {
             for (int x = 0; x < _bitmap.PixelWidth; x++)
             {
-                if (map.Map[x / 20, y / 40])
+                
+                var color = Normalize(HueToRgb(x), 255);
+                var ptr = _bitmap.BackBuffer + x * 4 + _bitmap.BackBufferStride * y;
+                unsafe
                 {
-                    var color = FromRgb(255, 255, 255);
-                    var ptr = _bitmap.BackBuffer + x * 4 + _bitmap.BackBufferStride * y;
-                    unsafe
-                    {
-                        *((int*)ptr) = (color.R << 16) | (color.G << 8) | (color.B);
-                    }
+                    *((int*)ptr) = (color.R << 16) | (color.G << 8) | (color.B);
                 }
             }
         }
-        _f++;
-        _bitmap.AddDirtyRect(new Int32Rect(0, 0, _bitmap.PixelHeight, _bitmap.PixelWidth));
+        _bitmap.AddDirtyRect(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight));
         _bitmap.Unlock();
     }
 
@@ -86,4 +120,11 @@ public partial class MainWindow : Window
             ? (byte)value
             : (byte)255
         : (byte)0;
+    private class ColorElement
+    {
+        public ColorElement()
+        {
+            
+        }
+    }
 }
